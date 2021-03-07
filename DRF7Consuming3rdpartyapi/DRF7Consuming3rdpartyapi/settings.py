0848783942a4c3d7f4481bce9ec6f6537340cd77
Janardhan Poola <jpoola@juniper.net>
2021-03-07 20:58:30 +0530

    debug false

--- DRF7Consuming3rdpartyapi/DRF7Consuming3rdpartyapi/settings.py
+++ DRF7Consuming3rdpartyapi/DRF7Consuming3rdpartyapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd7%bbvzhm&*wtxbzw=n)x=y&q2n2z47w_zno4q5ljms7)0+c#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
